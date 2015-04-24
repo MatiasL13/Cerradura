f0d0ec11373be2d3a6e335e63ee1d2beade759e9
Matias Sanchez <matias.l.sanchez@gmail.com>
2015-04-23 21:49:15 -0300

    inicio de actividad

---
+++ src/ar/edu/tallerWeb/Cerradura/Cerradura.java
@@ -0,0 +1,34 @@
+package ar.edu.tallerWeb.Cerradura;
+
+public class Cerradura {
+	int clave;
+	//boolean abierto = true;
+	public Cerradura(int clave)
+	{
+		this.clave = clave;
+	}
+	  boolean abrir(int clave) {
+		  
+	  }
+	  void cerrar() {
+		  
+	  }
+	  boolean estaAbierta() {
+		  
+	  }
+	  boolean estaCerrada() {
+		  
+	  }
+	  boolean fueBloqueada(){
+		  
+	  }
+	  int contarAperturasExitosas() {
+		  
+	  }
+	  int contarAperturasFallidas() {
+		  
+	  }
+	
+}
+
+
